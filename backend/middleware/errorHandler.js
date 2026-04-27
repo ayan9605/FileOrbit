561098f1ede2e0553b0f58edd5cebe495e58c526
ayan9605 <sumit334434@gmail.com>
2026-04-27 22:57:20 +0530

    Create errorHandler.js

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+// backend/middleware/errorHandler.js
+function errorHandler(err, req, res, next) {
+  console.error(err);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  const status = err.status || 500;
+  const message = err.message || 'Internal server error';
+
+  res.status(status).json({ message });
+}
+
+module.exports = errorHandler;
